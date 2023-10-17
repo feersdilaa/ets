8a0b7c2325b2ef49879647e7aa612cfd8497d080
feersdilaa <129427731+feersdilaa@users.noreply.github.com>
2023-10-17 08:46:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ function getWeather() {
           var version = document.getElementById('version');
           var namaOrang = document.getElementById('namaOrang');
           
-            namaOrang.textContent = data.name;
-            bio.textContent = data.bio;
-            language.textContent = data.language;
-            version.textContent = data.version;
-            id.textContent = data.id; 
+        namaOrang.textContent = data.name[1];
+        bio.textContent = data.bio;
+        language.textContent = data.language;
+        version.textContent = data.version;
+        id.textContent = data.id; 
         },
       error: function(err) {
         console.log(err);
